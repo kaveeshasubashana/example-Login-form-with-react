fd63c987e99785526ae384980b3c5ca793da39c2
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-24 00:20:14 +0530

    navbar edit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <h1>Login Page </h1>
+      <h1>Login Page and register </h1>
       <div>
         {!user && (
           <>
