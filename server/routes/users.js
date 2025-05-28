7c6adf9c8a0594e4d3c54e66586a3ef0a4dbe7bd
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-28 10:14:21 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { register, login } = require('../controllers/userController');
 
 router.post('/register', register);
-router.post('/login', login);  // ✅ This one!
+router.post('/login', login); 
 
 module.exports = router;
